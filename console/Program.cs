96f792f126268accf4b77e6e3d460246a6ec44e2
reinans <reinandesouza01@gmail.com>
2022-08-07 10:49:18 -0300

    feat: add console pratice

---
+++ console/Program.cs
@@ -0,0 +1,11 @@
+﻿using FluentColorConsole;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var text = ColorConsole.WithRedText;
+        text.WriteLine("Hello World");
+    }
+
+}
